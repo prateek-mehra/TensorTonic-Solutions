85dbd39f5119d2f14202f6c98433b1f17028bc89
Prateek Mehra <63323034+prateek-mehra@users.noreply.github.com>
2026-08-26 02:06:07 +0530

    Solve cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -1,37 +1,20 @@
 import numpy as np
-import math
-
-def eu_norm(x):
-
-    eu_norm = 0
-    for ele in x:
-        eu_norm = eu_norm + ele*ele
-
-    eu_norm = math.sqrt(eu_norm)
-
-    return eu_norm
 
 def cosine_similarity(a: list, b: list) -> float:
     """
     Returns the cosine similarity as a Python float.
     """
     # Write code here
+    
+    a = np.asarray(a, dtype = float)
+    b = np.asarray(b, dtype = float)
 
-    if eu_norm(a) == 0 or eu_norm(b) == 0:
+    a_norm = np.linalg.norm(a)
+    b_norm = np.linalg.norm(b)
+
+    if a_norm == 0 or b_norm == 0:
         return 0.0
 
-    dot_p = 0
-
-    n = len(a)
+    cos_sim = float(np.dot(a, b) / (a_norm * b_norm))
     
-    for i in range(n):
-        dot_p = dot_p + (a[i] * b[i])
-
-    dot_p = float(dot_p)
-
-    cos_sim = dot_p / eu_norm(a)
-    cos_sim = cos_sim / eu_norm(b)
-
     return cos_sim
-    
-    
